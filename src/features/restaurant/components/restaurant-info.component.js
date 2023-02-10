3422ad32c88414683576cca6cdaae587c4359322
Shreya K.C <shreyaa.k52@gmail.com>
2023-02-10 09:28:20 +0545

    initial commit

---
+++ src/features/restaurant/components/restaurant-info.component.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import {view,Text,StyleSheet} from 'react-native'
+
+export const RestaurantInfo = () =>{
+    return <Text> Restaurant Info will be made here</Text>
+};
+
+const styles = StyleSheet.create({});
--- src/features/restaurant/screens/restaurants.screen.js
+++ src/features/restaurant/screens/restaurants.screen.js
@@ -3,22 +3,25 @@ import { Text, StyleSheet, View, StatusBar } from "react-native";
 // import { Searchbar } from "react-native-paper";
 import { SafeAreaView } from "react-native-safe-area-context";
 import React, { useState } from "react";
+import { Searchbar } from "react-native-paper";
 
 
 
-export default function App() {
+export const RestaurantScreen = () {
 return (
+    <>
 <SafeAreaView style={styles.container}>
 <View style={{padding:16, backgroundColor: 'yellow'}}>
-<Text>Search</Text>
+<Searchbar/>
 </View>
 
 <View style={{flex:1, padding:16, backgroundColor:'red'}}>
-<Text>Resturant list</Text>
+
 </View>
 
 <StatusBar style = "auto" />
 </SafeAreaView>
+</>
 );
 }
 
